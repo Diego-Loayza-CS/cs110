ca66bb736210a6f6a267cb99b07685e1d6bf8ab7
Diego Loayza <diegoa.loayzall@gmail.com>
2024-12-01 00:16:27 -0500

    Progress - Wed Oct 9

--- unit3/homework3c/compare_lists.py
+++ unit3/homework3c/compare_lists.py
@@ -1,5 +1,50 @@
+def get_fruits():
+    fruit_list = []
+    while True:
+        fruit = input("Enter a Fruit: ")
+        if fruit == "":
+            break
+        fruit_list.append(fruit)
+    return fruit_list
+
+
+def get_vegetables():
+    vegetables_list = []
+    while True:
+        vegetable = input("Enter a Vegetable: ")
+        if vegetable == "":
+            break
+        vegetables_list.append(vegetable)
+    return vegetables_list
+
+
+def print_vegetables(vegetables):
+    print("Vegetables:")
+    for vegetable in vegetables:
+        print(f" - {vegetable}")
+
+
+def print_fruits(fruits):
+    print("Fruits:")
+    for fruit in fruits:
+        print(f" - {fruit}")
+
+
+def print_recommendation(vegetables, fruits):
+    if len(vegetables) > len(fruits):
+        print("You need more fruit!")
+    elif len(vegetables) < len(fruits):
+        print("You need more vegetables!")
+    else:
+        print("What a healthy balanced diet!")
+
+
 def main():
-    pass
+    fruits = get_fruits()
+    vegetables = get_vegetables()
+    print_fruits(fruits)
+    print_vegetables(vegetables)
+    print_recommendation(vegetables, fruits)
 
 
 if __name__ == '__main__':
--- unit3/homework3c/custom_bullets.py
+++ unit3/homework3c/custom_bullets.py
@@ -9,13 +9,28 @@ def get_items():
 
 
 def get_bullet_points():
-    pass
+    bullet_list = []
+    while True:
+        bullet = input("Custom Bullet Point: ")
+        if bullet == "":
+            break
+        bullet_list.append(bullet)
+    return bullet_list
+
+
+def print_lists(items, bullet_points):
+    for bullet in bullet_points:
+        for item in items:
+            print(f"{bullet} {item}")
+        print()
 
 
 def main():
     items = get_items()
     print()
     bullet_points = get_bullet_points()
+    print()
+    print_lists(items, bullet_points)
 
 
 if __name__ == '__main__':
